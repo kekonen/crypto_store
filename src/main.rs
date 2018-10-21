5d057ef223b1e57c955416393acf305acb9ef0e5
kekonen <daniil.naumetc@gmail.com>
2018-10-21 14:20:27 +0200

    trades orders ticker

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ use rocket_contrib::{Json};
 #[derive(Serialize, Deserialize, Debug)]
 struct Trade {
     side: bool, // sell: 1 true
-    price: u64,
-    amount: u64,
+    price: f64,
+    amount: f64,
     ts: u64,
     id: u64
 }
@@ -23,39 +23,59 @@ struct TradeList {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Order {
-    amount: u64,
-    price: u64
+struct OrderLevel {
+    amount: f64,
+    price: f64
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct OrdersList {
-    bids: Vec<Order>,
-    asks: Vec<Order>,
+    bids: Vec<OrderLevel>,
+    asks: Vec<OrderLevel>,
     ts: u64
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct OrdersList {
-    bids: Vec<Order>,
-    asks: Vec<Order>,
+struct Ticker {
+    symbol: String,
+    high: f64,
+    low: f64,
+    bid: f64,
+    ask: f64,
+    open: f64,
+    close: f64,
+    last: f64,
     ts: u64
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TickerList {
+    tickers: Vec<Ticker>
 }
 
 
 #[post("/trades/<exchange>/<ticker>", format = "application/json", data = "<trades>")]
 fn trades(exchange: String, ticker: String, trades: Json<TradeList>) -> String {
-    format!("Incoming, exchange:{}, ticker {}, with data: {}!", exchange, ticker, trades.trades[0].description)
+    let answer = format!("Incoming, exchange:{}, ticker {}, with data: {}!", exchange, ticker, trades.trades[0].ts);
+    println!("{}", answer);
+    answer
 }
 
 #[post("/orders/<exchange>/<ticker>", format = "application/json", data = "<orders>")]
-fn trades(exchange: String, ticker: String, orders: Json<orderList>) -> String {
-    format!("Incoming, exchange:{}, ticker {}, with data: {}!", exchange, ticker, trades.trades[0].description)
+fn orders(exchange: String, ticker: String, orders: Json<OrdersList>) -> String {
+    let answer = format!("Incoming, exchange:{}, ticker {}, with data: {}!", exchange, ticker, orders.ts);
+    println!("{}", answer);
+    answer
 }
 
-#[post("/orders/<exchange>/<ticker>", format = "application/json", data = "<orders>")]
-fn trades(exchange: String, ticker: String, orders: Json<orderList>) -> String {
-    format!("Incoming, exchange:{}, ticker {}, with data: {}!", exchange, ticker, trades.trades[0].description)
+#[post("/ticker/<exchange>", format = "application/json", data = "<ticker>")]
+fn ticker(exchange: String, ticker: Json<Ticker>) -> String {
+    format!("Incoming, exchange:{}, with data: {}!", exchange, ticker.symbol)
+}
+
+#[post("/tickers/<exchange>", format = "application/json", data = "<tickers>")]
+fn tickers(exchange: String, tickers: Json<TickerList>) -> String {
+    format!("Incoming, exchange:{}, with data: {}!", exchange, tickers.tickers[0].symbol)
 }
 
 fn main() {
