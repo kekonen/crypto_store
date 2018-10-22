4ef056a761b1be458b1684f230d6dbf201b2c6e5
kekonen <daniil.naumetc@gmail.com>
2018-10-22 18:55:59 +0200

    trades works cool

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@
 
 #[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+extern crate time;
+
 
 extern crate rocket;
 
@@ -11,12 +15,40 @@ use rocket_contrib::{Json};
 use std::collections::{VecDeque};
 use std::collections::HashMap;
 use std::sync::Mutex;
+use std::error::Error;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+
+fn get_time() -> String {
+    let kek = time::now();
+    return format!("{}-{}-{}-{}-{}",kek.tm_hour, kek.tm_min, kek.tm_mday, kek.tm_mon, kek.tm_year+1900);
+}
+
+fn write_file(path_way: &String, contents: &String) -> () {
+    let path = Path::new(path_way);
+
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}", why.description()),
+        Ok(file) => file,
+    };
+
+    match file.write_all(contents.as_bytes()) {
+        Err(why) => {
+            panic!("couldn't write ")
+        },
+        Ok(_) => println!("successfully wrote to"),
+    }
+}
 
 struct TradesHandler {
     // exchanges: String,
     container: HashMap<String, Vec<Trade>>,
     path: String
 }
+
+type TradesHandlerMut = Mutex<TradesHandler>;
+
 
 impl TradesHandler {
     fn new(exchanges: Vec<String>, tickers: Vec<String>, path: String) -> TradesHandler {
@@ -50,22 +82,65 @@ impl TradesHandler {
             let trades_length = trades.len();
             println!("Container cap: {}, container length: {}, trades_length: {}", capacity, length, trades_length);
             if (length + trades_length) > capacity {
-                self.release(exchange, ticker).unwrap();
-            }
+                let mut future_file = String::from("");
+                for trade in container.clone() {
+                    future_file.push('\n');
+                    future_file.push_str(&serde_json::to_string(&trade).unwrap());
+                }
 
-            // make check batch
-            for &trade in trades {
-                // implement equal to trade, and compare latest ${trades_length or length or 0} on copies and not push if exists, pay attention on that the latest batch(see: 56 "checkbatch")
-                container.push(trade.clone());
+                println!("full... writing file for key: {}", key);
+                write_file(&format!("out/{}-{}.txt", key, get_time()), &future_file);
+                
+                container.clear();
+
+                for &trade in trades {
+                    // let trade_clone = trade.clone();
+                    // println!("Pushing...");
+                    container.push(trade.clone())
+                }
+                // let path = Path::new();
+
+
+                // self.release(exchange, ticker).unwrap();
+            } else {
+                // make check batch
+                let container_clone = container.clone();
+                if length == 0 {
+                    for &trade in trades {
+                        // let trade_clone = trade.clone();
+                        // println!("Pushing...");
+                        container.push(trade.clone())
+                    }
+                } else {
+                    let min_length = if length < 100 {length} else {100}; 
+                    let check_batch = &container_clone[..min_length];
+                    for &trade in trades {
+                        // implement equal to trade, and compare latest ${trades_length or length or 0} on copies and not push if exists, pay attention on that the latest batch(see: 56 "checkbatch")
+                        let trade_clone = trade.clone();
+                        match check_batch.iter().find(|&&tr| tr == trade_clone) {
+                            Some(_) => {
+                                // println!("Found clone {}", &serde_json::to_string(&trade_clone).unwrap());
+                                continue;
+                            },
+                            None => container.push(trade_clone),
+                        }
+                        // println!("Pushing...");
+                        
+                    }
+                }
+                
             }
+            println!(" container.len {}, key {}", container.len(), key);
+            
         }
     }
 
-    fn release(&mut self, exchange: String, ticker: String) -> Option {
-        //serialize vector of trades
-        //push the file into the folder
-        //clean the container
-    }
+    // fn release(&mut self, exchange: String, ticker: String) -> Option<()> {
+    //     //serialize vector of trades
+    //     //push the file into the folder
+    //     //clean the container
+    //     Some(())
+    // }
 
 }
 
@@ -73,7 +148,7 @@ impl TradesHandler {
 
 ///
 
-#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
+#[derive(Serialize, Deserialize, Debug, Copy, Clone, PartialEq)]
 struct Trade {
     side: bool, // sell: 1 true
     price: f64,
@@ -127,18 +202,18 @@ type HandlersMut = Mutex<Handlers>;
 
 
 #[post("/trades/<exchange>/<ticker>", format = "application/json", data = "<trades>")]
-fn trades(handlers: State<HandlersMut>, exchange: String, ticker: String, trades: Json<TradeList>) -> String {
+fn trades(trades_handler: State<TradesHandlerMut>, exchange: String, ticker: String, trades: Json<TradeList>) -> String {
     let answer = format!("Incoming, exchange:{}, ticker {}, with data: {}!", exchange, ticker, trades.trades[0].ts);
-    println!("{}", answer);
-    let mut mutHandlers = handlers.lock().unwrap();
-    mutHandlers.tradesHandler.push(exchange, ticker, &trades.trades);
+    // println!("{}", answer);
+    let mut mut_handler = trades_handler.lock().unwrap();
+    mut_handler.push(exchange, ticker, &trades.trades);
     answer
 }
 
 #[post("/orders/<exchange>/<ticker>", format = "application/json", data = "<orders>")]
 fn orders(exchange: String, ticker: String, orders: Json<OrdersList>) -> String {
     let answer = format!("Incoming, exchange:{}, ticker {}, with data: {}!", exchange, ticker, orders.ts);
-    println!("{}", answer);
+    // println!("{}", answer);
     answer
 }
 
@@ -181,7 +256,9 @@ fn main() {
     
     rocket::ignite()
     .mount("/", routes![trades, orders, ticker])
-    .manage(Mutex::new(Handlers { tradesHandler: TradesHandler::new(exchanges, tickers, path)}))
+    .manage(Mutex::new(TradesHandler::new(exchanges, tickers, path)))
     .launch();
     
 }
+
+    // .manage(Mutex::new(Handlers { tradesHandler: TradesHandler::new(exchanges, tickers, path)}))
